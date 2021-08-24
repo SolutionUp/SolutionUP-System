121deb176f139e09eccfc38a4e08e355a4ad4258
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-24 01:29:06 -0300

    Cria estilo para os links ativos do menu

--- static/js/geral.js
+++ static/js/geral.js
@@ -1,3 +1,12 @@
+// Estilo para os links ativos da navbar
+document.querySelectorAll('.nav-link').forEach(function(elemento){ 
+    if(elemento.pathname.split('/')[1] == window.location.pathname.split('/')[1] ){
+        elemento.classList.add('atual')
+    }
+    else{
+        elemento.classList.remove('atual')
+    }})
+
 // Esconde as mensagens após um tempo
 setTimeout(function() {
     document.querySelector('.messages').style.display = 'none'
