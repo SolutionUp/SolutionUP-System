2174bf082b04914594d65c594a7c3a509433143b
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-24 01:28:05 -0300

    Cria rota principal para listagem dos produtos

--- solutionup/urls.py
+++ solutionup/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('estoque_produtos.urls')),
     path('produtos/', include('estoque_produtos.urls')),
 ]
