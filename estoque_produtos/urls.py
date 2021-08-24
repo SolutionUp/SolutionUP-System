519d5016641ae919d5ba41d736e91f4cce4c2233
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-24 01:24:10 -0300

    Ajusta rota de listagem dos produtos

--- estoque_produtos/urls.py
+++ estoque_produtos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.ProdutoListView.as_view(), name="listagem"),
-    path('<int:pk>/', views.ProdutoDetailView.as_view(), name="detalhe"),
-    path('adicionar', views.adicionar_produto, name="adicionar_produto"),
-    path('alterar/<int:codigo>', views.alterar_produto, name="alterar_produto"),
+    path('', views.ProdutoListView.as_view(), name='listagem_produto'),
+    path('<int:pk>/', views.ProdutoDetailView.as_view(), name='detalhe_produto'),
+    path('adicionar', views.adicionar_produto, name='adicionar_produto'),
+    path('alterar/<int:codigo>', views.alterar_produto, name='alterar_produto'),
     path('remover/<int:codigo>', views.remover_produto, name='remover_produto'),
 ]
