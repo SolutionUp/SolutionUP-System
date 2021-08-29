a36a74d2f0b14b15dda45e2ee211c05d4fad4ce0
Emerson Soares <emerson.soares@aluno.faculdadeimpacta.com.br>
2021-08-29 00:12:34 -0300

    Cria preview da imagem do produto

--- static/js/geral.js
+++ static/js/geral.js
@@ -11,3 +11,23 @@ document.querySelectorAll('.nav-link').forEach(function(elemento){
 setTimeout(function() {
     document.querySelector('.messages').style.display = 'none'
 }, 3000)
+
+// Renderiza as imagens antes de salvar
+$('#id_imagem').change(function(){
+    elemento = $('#id_imagem')[0]
+    if (elemento.files && elemento.files[0]) {
+        var src = URL.createObjectURL(elemento.files[0])
+        $('#image').attr('src', src)
+        $('#image').removeClass('d-none')
+        $('#image_').addClass('d-none')
+    }
+    else{
+        $('#image_').removeClass('d-none')
+        $('#image').addClass('d-none')
+    }
+})
+
+function resetar_imagem(){
+    $("#id_imagem").val(null);
+    $("#id_imagem").trigger("change")
+}
