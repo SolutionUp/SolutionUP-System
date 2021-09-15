a6dd8fbfc0ba4406957c235556b449f5015989de
IgorImpacta <igor.teixeira@aluno.faculdadeimpacta.com.br>
2021-09-15 15:00:49 -0300

    Ativa painel administrativo

--- solutionup/urls.py
+++ solutionup/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls), 
     path('', include('produtos.urls')),
     path('produtos/', include('produtos.urls.produtos_url')),
     path('fornecedores/', include('produtos.urls.fornecedor_url')),
